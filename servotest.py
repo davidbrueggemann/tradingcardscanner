f873f70ce1ced1b2850c2a8c76c09ddc1a6e1a99
David B <david.brueggemann@gebit.de>
2020-10-02 14:49:41 +0200

    Add comments to servo test and add asw upload script

--- servotest.py
+++ servotest.py
@@ -1,9 +1,12 @@
+####### IMPORTS ##########
 from PIL import Image
 from picamera import PiCamera
 import RPi.GPIO as GPIO
 import time
 import sys
+import awsupload # own file
 
+####### SETUP ##########
 def camera_setup(camera):
 	camera.color_effects = (128,128)
 	camera.rotation = 0
@@ -23,14 +26,17 @@ setup()
 camera = PiCamera()
 camera_setup(camera)
 
+####### START ##########
 print "initial Servos on PINs with 50 hz"
 servo=GPIO.PWM(servoPin,50)
 miniServo=GPIO.PWM(miniServoPin,50)
 miniServo.start(2.5)
 time.sleep(1)
 
+####### PROCESS LOOP ##########
 print "Going into loop"
 while True:
+	### CARD MOVEMENT SERVO ####
 	print "Wheel Servo left start"
 	servo.start(15)
 	print "Waiting for 1 sec"
@@ -40,11 +46,18 @@ while True:
 	print "Waiting for 1 sec"
 	time.sleep(1)
 
-	#timestamp=time.strftime("%Y%m%d%H%M%S")
-	#camera.capture("testfoto"+"_"+timestamp+".jpg")
-	camera.capture("testfoto"+".jpg")
+	### CAMERA ###
+	timestamp=time.strftime("%Y%m%d%H%M%S")
+	picFileName="testfoto"+"_"+timestamp+".jpg"
+	camera.capture(picFileName)
+	time.sleep(1)
 	print "Picture Taken!" # See {0}_{1}.jpg!".format(set_name,timestamp)
 
+	### UPLOAD TO AWS ###
+	isUploaded=awsupload.upload_file(picFileName)
+	print("Upload to AWS successful.") if isUploaded else print("Upload to AWS failed!")
+
+	### CARD STOPPER SERVO ####
 	print "Little Servo open (90degrees to the side)"
 	miniServo.ChangeDutyCycle(7.5)
 	print "Waiting for 1 sec"
@@ -54,4 +67,5 @@ while True:
 	print "Waiting for 1 sec"
 	time.sleep(1)
 
+####### CLEANUP ##########
 GPIO.cleanup()
